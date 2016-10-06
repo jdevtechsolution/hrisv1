0d583dea4926140a3593deb8989c814b2f2ccce4
Jb <iamjbpv@outlook.com>
2016-10-06 19:45:56 +0800

    forgot to add files

--- application/views/employee_view.php
+++ application/views/employee_view.php
@@ -2184,6 +2184,7 @@ $(document).ready(function(){
                 hideRatesduties();
                 hideEntitlement();
                 showApplyLeave();
+                showSpinningProgressLoading();
                 getFiledLeave();
                 
             }
